c919e994d370058dc760c32d874240d7ca4d3eeb
DELL <jimsoft@qq.com>
2023-06-11 10:03:03 +0800

    test

--- routers/routers.go
+++ routers/routers.go
@@ -2,11 +2,13 @@ package routers
 
 import (
 	"dbOper/controller"
+	"dbOper/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func SetRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(middleware.CORSMiddleware())
 	/**
 	用户User路由组
 	*/
